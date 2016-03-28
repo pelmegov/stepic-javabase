3e46d96cdff29eb651d74d0421c9457c96571edc
stepandarya <modkomi@mail.ru>
2016-03-28 17:24:15 +0300

    Initial commit

---
+++ src/main/java/module_2/lesson_2_1/step_8/Main.java
@@ -0,0 +1,16 @@
+package module_2.lesson_2_1.step_8;
+
+/**
+ * @author modkomi
+ * @since 28.03.2016
+ */
+public class Main {
+    public static long longExpression(int a, int b, int c) {
+        long one_milliard = 100_000_000_000L;
+        return one_milliard;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longExpression(843, 0, 5));
+    }
+}
