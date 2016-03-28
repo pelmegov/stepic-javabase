c5964bdea9a1b9492cdc93ff8e35daea19d65725
stepandarya <modkomi@mail.ru>
2016-03-28 18:28:42 +0300

    added realization

--- src/main/java/module_2/lesson_2_1/step_8/Main.java
+++ src/main/java/module_2/lesson_2_1/step_8/Main.java
@@ -22,9 +22,9 @@ package module_2.lesson_2_1.step_8;
  * @since 28.03.2016
  */
 public class Main {
+
     public static long longExpression(int a, int b, int c) {
-        long one_milliard = 100_000_000_000L;
-        return one_milliard;
+        return (100_000_000_000L % a >> b) | (100 / c);
     }
 
     public static void main(String[] args) {
